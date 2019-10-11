22dd1027867eb2480b9245472d4710d0f4f9151f
William Billingsley <wbilling@une.edu.au>
2019-10-11 21:43:44 +1100

    Slight cleanup to NumberInputTile

--- scatter/src/main/scala/com/wbillingsley/scatter/jstiles/NumberInputTile.scala
+++ scatter/src/main/scala/com/wbillingsley/scatter/jstiles/NumberInputTile.scala
@@ -6,28 +6,30 @@ import com.wbillingsley.veautiful.{<, DElement, DiffComponent, PropVal, ^}
 import org.scalajs.dom
 import org.scalajs.dom.raw.HTMLInputElement
 
-class NumberInputTile (tileSpace:TileSpace[JSExpr], width:Int = 2, initial:Option[Double] = None) extends Tile(tileSpace) with DiffComponent {
+  class NumberInputTile (tileSpace:TileSpace[JSExpr], width:Int = 2, initial:Option[Double] = None) extends Tile(tileSpace) with DiffComponent {
 
   import NumberInputTile._
 
   var number:Option[Double] = initial
 
-  val input = <.input(^.attr("type") := "Number", ^.cls := "", ^.on("input") ==> setNumber, ^.on("pointerdown") --> {})
+  val input = <.input(^.attr("type") := "Number", ^.cls := "", ^.on("input") ==> onInput)
+
+  def setNumber(n:Double):Unit = {
+    number = Some(n)
+    input.makeItSo(<.input(^.attr("type") := "Number", ^.cls := "", ^.on("input") ==> onInput, ^.prop("value") ?= number.map(_.toString)))
+  }
 
   override def afterAttach(): Unit = {
     super.afterAttach()
   }
 
-  def setNumber(e:dom.Event):Unit = {
-    number = try {
-      val on = Some(e.target.asInstanceOf[HTMLInputElement].value.toDouble)
-      for {
-        n <- on
-      } input.prop(PropVal("value", n.toString))
-      on
+  def onInput(e:dom.Event):Unit = {
+    try {
+      setNumber(e.target.asInstanceOf[HTMLInputElement].value.toDouble)
     } catch {
-      case x:Exception => number
+      case _:Exception => // Number format
     }
+    rerender()
   }
 
   override val tileContent: TileComponent[JSExpr] = HBox(
