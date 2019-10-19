a20b8f3384838499b2537ad2a938f666d38cf23d
William Billingsley <wbillingsley@cantab.net>
2019-10-19 20:20:55 +1100

    Added VoltageSource

--- docs/src/main/scala/example/WrenExample.scala
+++ docs/src/main/scala/example/WrenExample.scala
@@ -1,13 +1,14 @@
 package example
 
 import com.wbillingsley.veautiful.{<, VNode, ^}
-import com.wbillingsley.wren.{Circuit, CurrentSource, Orientation, Resistor}
+import com.wbillingsley.wren.{Circuit, CurrentSource, Orientation, Resistor, VoltageSource}
 
 object WrenExample {
 
   val circuit = Circuit(Seq(
     new CurrentSource((100, 100)),
-    new Resistor((200,100), orientation = Orientation.South)
+    new Resistor((200,100), orientation = Orientation.South),
+    new VoltageSource((300, 100), orientation = Orientation.East)
   ), 640, 480)
 
   def page:VNode =  Common.layout(<.div(^.cls := "row",
