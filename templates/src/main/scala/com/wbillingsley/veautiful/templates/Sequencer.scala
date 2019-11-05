457995d752735fb66a3f78e85ecbbce43158d420
William Billingsley <wbilling@une.edu.au>
2019-11-05 19:08:10 +1100

    Started adding some capability for Slides and sequence items to control the "footbox" buttons

--- templates/src/main/scala/com/wbillingsley/veautiful/templates/Sequencer.scala
+++ templates/src/main/scala/com/wbillingsley/veautiful/templates/Sequencer.scala
@@ -11,21 +11,24 @@ object Sequencer {
     ))
   }
 
-
 }
 
-case class Sequencer(override val key:Option[String] = None)(var nodes: Seq[VNode], var index:Int = 0) extends DiffComponent with MakeItSo {
+case class Sequencer(override val key:Option[String] = None)(var nodes: Seq[SequenceItem], var index:Int = 0) extends DiffComponent with MakeItSo {
 
   def next():Unit = {
     if (index < nodes.size - 1) {
+      nodes(index).active = false
       index = index + 1
+      nodes(index).active = true
       update()
     }
   }
 
   def previous():Unit = {
     if (index > 0) {
+      nodes(index).active = false
       index = index - 1
+      nodes(index).active = true
       update()
     }
   }
@@ -47,7 +50,7 @@ case class Sequencer(override val key:Option[String] = None)(var nodes: Seq[VNod
         n
       )
     ),
-    footBox
+    if (nodes.nonEmpty && nodes(index).showFootBox()) footBox else <.div()
   )
 
   override def makeItSo: PartialFunction[MakeItSo, _] = {
@@ -56,4 +59,32 @@ case class Sequencer(override val key:Option[String] = None)(var nodes: Seq[VNod
       index = s.index
       rerender()
   }
+}
+
+
+class SequenceItem(
+  var content:VNode,
+  override val key: Option[String] = None,
+  val readyForward: () => Boolean = { () => true },
+  val enableForward: () => Boolean = { () => true },
+  val readyBack: () => Boolean = { () => true },
+  val enableBack: () => Boolean = { () => true },
+  val showFootBox: () => Boolean = { () => true }
+) extends DiffComponent {
+
+  var active:Boolean = false
+
+  override protected def render: DiffNode = <.div(
+    ^.cls := "v-slide",
+    content
+  )
+
+}
+
+object SequenceItem {
+
+  implicit def lift(v:VNode):SequenceItem = new SequenceItem(v)
+
+  implicit def lift(vs:Seq[VNode]):Seq[SequenceItem] = vs.map(v => lift(v))
+
 }
--- templates/src/main/scala/com/wbillingsley/veautiful/templates/VSlides.scala
+++ templates/src/main/scala/com/wbillingsley/veautiful/templates/VSlides.scala
@@ -4,7 +4,7 @@ import com.wbillingsley.veautiful.{<, DiffComponent, DiffNode, MakeItSo, VNode, 
 import org.scalajs.dom
 import org.scalajs.dom.raw.{Event, HTMLElement}
 
-case class VSlides(width: Int, height: Int, override val key: Option[String] = None)(var content:Seq[VNode], var index:Int = 0) extends DiffComponent with MakeItSo {
+case class VSlides(width: Int, height: Int, override val key: Option[String] = None)(var content:Seq[SequenceItem], var index:Int = 0) extends DiffComponent with MakeItSo {
 
   var scale:Double = 1
   var top:Double = 0
@@ -45,7 +45,7 @@ case class VSlides(width: Int, height: Int, override val key: Option[String] = N
     <.div(^.cls := "vslides-top",
       <.div(^.cls := "vslides-scaler", ^.attr("style") := s"transform: scale($scale); width: ${width}px; height: ${height}px; top: ${top}px; left: ${left}px; ",
         Sequencer()(
-          content.map(slide), index
+          content, index
         )
       )
     )
