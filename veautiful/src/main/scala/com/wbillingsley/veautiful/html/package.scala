0972fd8bac8d550c1ad69337324eceec8c75d06c
William Billingsley <wbillingsley@cantab.net>
2020-04-25 18:00:21 +1000

    Add EventMethods
    These are a couple of useful extension methods for events in event handlers for dealing with input elements.

--- veautiful/src/main/scala/com/wbillingsley/veautiful/html/package.scala
+++ veautiful/src/main/scala/com/wbillingsley/veautiful/html/package.scala
@@ -1,6 +1,7 @@
 package com.wbillingsley.veautiful
 
 import org.scalajs.dom
+import org.scalajs.dom.{Event, html}
 
 package object html {
 
@@ -9,5 +10,23 @@ package object html {
   type VHtmlDiffNode = DiffNode[dom.Element, dom.Node]
 
   type VHtmlComponent = DiffComponent[dom.Element, dom.Node]
+
+  /**
+    * Extension functions on Event that are useful for writing input elements
+    */
+  implicit class EventMethods(val e:Event) extends AnyVal {
+
+    def inputValue:Option[String] = e.target match {
+      case h:html.Input => Some(h.value)
+      case t:html.TextArea => Some(t.value)
+      case _ => None
+    }
+
+    def checked:Option[Boolean] = e.target match {
+      case h:html.Input => Some(h.checked)
+      case _ => None
+    }
+
+  }
 
 }
